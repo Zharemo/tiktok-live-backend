400867e28033c3a17736fd80cb68a7be3d034d7d
Zharemo <isapenninowork@gmail.com>
2025-04-06 16:05:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const ConnectionWrapper = require('./connectionWrapper');
+const ConnectionWrapper = require('./ConnectionWrapper');
 const RateLimiter = require('./limiter');
 
 const app = express();
