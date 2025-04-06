6a8e8855db207f41a184d139b8a11e268774ef9e
Zharemo <isapenninowork@gmail.com>
2025-04-06 17:26:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -92,6 +92,6 @@ setInterval(() => {
 app.use(express.static('public'));
 
 // Start http listener
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 8081;
 httpServer.listen(port);
 console.info(`Server running! Please visit http://localhost:${port}`);
