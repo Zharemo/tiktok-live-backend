955b1b0084b62a03e6942cd2d026cfcdaeeeecf1
Zharemo <isapenninowork@gmail.com>
2025-04-08 14:18:39 +0200

    Update connectionWrapper.js

--- connectionWrapper.js
+++ connectionWrapper.js
@@ -1,125 +1,36 @@
 const { WebcastPushConnection } = require('tiktok-live-connector');
-const { EventEmitter } = require('events');
+const EventEmitter = require('events');
 
-let globalConnectionCount = 0;
+class ConnectionWrapper extends EventEmitter {
+  constructor(username, options = {}) {
+    super();
+    this.connection = new WebcastPushConnection(username, options);
+    this.username = username;
+    this.eventNamesToListen = ['chat', 'gift', 'follow', 'like', 'share', 'subscribe', 'member', 'roomUser', 'tiktokConnected'];
 
-/**
- * TikTok LIVE connection wrapper with advanced reconnect functionality and error handling
- */
-class TikTokConnectionWrapper extends EventEmitter {
-    constructor(uniqueId, options, enableLog) {
-        super();
+    this.eventNamesToListen.forEach(event => {
+      this.connection.on(event, (data) => {
+        this.emit('event', event, data);
+      });
+    });
 
-        this.uniqueId = uniqueId;
-        this.enableLog = enableLog;
+    this.connection.on('disconnected', () => {
+      console.warn('Disconnected from TikTok live');
+    });
+  }
 
-        // Connection State
-        this.clientDisconnected = false;
-        this.reconnectEnabled = true;
-        this.reconnectCount = 0;
-        this.reconnectWaitMs = 1000;
-        this.maxReconnectAttempts = 5;
-
-        this.connection = new WebcastPushConnection(uniqueId, options);
-
-        this.connection.on('streamEnd', () => {
-            this.log(`streamEnd event received, giving up connection`);
-            this.reconnectEnabled = false;
-        })
-
-        this.connection.on('disconnected', () => {
-            globalConnectionCount -= 1;
-            this.log(`TikTok connection disconnected`);
-            this.scheduleReconnect();
-        });
-
-        this.connection.on('error', (err) => {
-            this.log(`Error event triggered: ${err.info}, ${err.exception}`);
-            console.error(err);
-        })
+  async connect(username) {
+    try {
+      if (this.connection.isConnected()) {
+        await this.connection.disconnect();
+      }
+      this.connection.username = username;
+      await this.connection.connect();
+      console.log(`Connected to ${username}`);
+    } catch (err) {
+      console.error('Failed to connect:', err);
     }
-
-    connect(isReconnect) {
-        this.connection.connect().then((state) => {
-            this.log(`${isReconnect ? 'Reconnected' : 'Connected'} to roomId ${state.roomId}, websocket: ${state.upgradedToWebsocket}`);
-
-            globalConnectionCount += 1;
-
-            // Reset reconnect vars
-            this.reconnectCount = 0;
-            this.reconnectWaitMs = 1000;
-
-            // Client disconnected while establishing connection => drop connection
-            if (this.clientDisconnected) {
-                this.connection.disconnect();
-                return;
-            }
-
-            // Notify client
-            if (!isReconnect) {
-                this.emit('connected', state);
-            }
-
-        }).catch((err) => {
-            this.log(`${isReconnect ? 'Reconnect' : 'Connection'} failed, ${err}`);
-
-            if (isReconnect) {
-                // Schedule the next reconnect attempt
-                this.scheduleReconnect(err);
-            } else {
-                // Notify client
-                this.emit('disconnected', err.toString());
-            }
-        })
-    }
-
-    scheduleReconnect(reason) {
-
-        if (!this.reconnectEnabled) {
-            return;
-        }
-
-        if (this.reconnectCount >= this.maxReconnectAttempts) {
-            this.log(`Give up connection, max reconnect attempts exceeded`);
-            this.emit('disconnected', `Connection lost. ${reason}`);
-            return;
-        }
-
-        this.log(`Try reconnect in ${this.reconnectWaitMs}ms`);
-
-        setTimeout(() => {
-            if (!this.reconnectEnabled || this.reconnectCount >= this.maxReconnectAttempts) {
-                return;
-            }
-
-            this.reconnectCount += 1;
-            this.reconnectWaitMs *= 2;
-            this.connect(true);
-
-        }, this.reconnectWaitMs)
-    }
-
-    disconnect() {
-        this.log(`Client connection disconnected`);
-
-        this.clientDisconnected = true;
-        this.reconnectEnabled = false;
-
-        if (this.connection.getState().isConnected) {
-            this.connection.disconnect();
-        }
-    }
-
-    log(logString) {
-        if (this.enableLog) {
-            console.log(`WRAPPER @${this.uniqueId}: ${logString}`);
-        }
-    }
+  }
 }
 
-module.exports = {
-    TikTokConnectionWrapper,
-    getGlobalConnectionCount: () => {
-        return globalConnectionCount;
-    }
-};
+module.exports = ConnectionWrapper;
